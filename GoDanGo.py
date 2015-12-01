e26a7d22730e83f4a55ecd8a880ccfa038fa6417
Anthony Palombo <palomboa@gilmour.org>
2015-12-01 10:06:25 -0500

    Added a new Servo check.

--- GoDanGo.py
+++ GoDanGo.py
@@ -153,7 +153,7 @@ MaterPi = Pigo()
 while MaterPi.keepGoing():
     MaterPi.checkDist()
 ### Put the method call after MaterPi here in order to tell the robot what to do.
-    MaterPi.safeDrive()
+    MaterPi.fwd()
     while MaterPi.keepGoing():
         MaterPi.checkDist()
     MaterPi.stop()
